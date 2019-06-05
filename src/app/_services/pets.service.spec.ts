c16d7eaa70756f1815317f6bb0f52fe82f9aff57
RD-HQ\abhishek <abhishek.lal@roadhouse.com.au>
2019-06-05 14:21:24 +1000

    test scenarios for services

--- src/app/_services/pets.service.spec.ts
+++ src/app/_services/pets.service.spec.ts
@@ -1,12 +1,54 @@
-import { TestBed } from '@angular/core/testing';
-
-import { PetsService } from './pets.service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+import {PetsService} from './pets.service';
+import {of} from 'rxjs';
 
 describe('PetsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let petsService: PetsService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        PetsService,
+        {provide: HttpClient, useValue: spy}
+      ]
+    });
+
+    // Inject both the service-to-test and its (spy) dependency
+    petsService = TestBed.get(PetsService);
+    httpClientSpy = TestBed.get(HttpClient);
+  });
 
   it('should be created', () => {
-    const service: PetsService = TestBed.get(PetsService);
-    expect(service).toBeTruthy();
+    petsService = new PetsService(httpClientSpy);
+    expect(petsService).toBeTruthy();
+  });
+
+  it('#getPetsData should return any stubbed value from a httpClient service', () => {
+
+    const stubValue = of({});
+    httpClientSpy.get.and.returnValue(stubValue);
+
+    petsService = new PetsService(httpClientSpy);
+    expect(petsService.getPetsData()).toBe(stubValue, 'service returned stub value');
+  });
+
+  it('#getPetsData should return an observable value from a httpClient service', () => {
+
+    const stubValue = of([
+      {name: 'Bob', gender: 'Male', age: 23, pets: [{name: 'Garfield', type: 'Cat'}, {name: 'Fido', type: 'Dog'}]},
+      {name: 'Bob2', gender: 'Male', age: 23, pets: [{name: 'garfield', type: 'Cat'}, {name: 'fido', type: 'Dog'}]}
+    ]);
+
+    httpClientSpy.get.and.returnValue(stubValue);
+
+    petsService = new PetsService(httpClientSpy);
+    expect(petsService.getPetsData()).actual.subscribe(result => {
+      expect(result[0].name).toBe('Bob', 'getPetsData returned observable value');
+    });
   });
 });
--- src/app/shared/auth-guard.service.spec.ts
+++ src/app/shared/auth-guard.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthGuardService } from './auth-guard.service';
+import {Router} from '@angular/router';
 
 describe('AuthGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let authGuardService: AuthGuardService;
+  let routerSpy: jasmine.SpyObj<Router>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuardService,
+        {provide: Router}
+      ]
+    });
+
+    // Inject both the service-to-test and its (spy) dependency
+    authGuardService = TestBed.get(AuthGuardService);
+    routerSpy = TestBed.get(Router);
+  });
 
   it('should be created', () => {
     const service: AuthGuardService = TestBed.get(AuthGuardService);
     expect(service).toBeTruthy();
+  });
+
+  it('canActivate should always return true', () => {
+    const service: AuthGuardService = TestBed.get(AuthGuardService);
+    expect(service.canActivate({} as any, {} as any)).toBe(true, 'canActivate returns true always');
   });
 });
